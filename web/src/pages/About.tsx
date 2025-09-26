86a32dcaa46730d78df80dfbf337013fcd3021b2
reejung.kim <reejung.kim@gmail.com>
2025-09-27 00:00:46 +0900

    Add task master working

---
+++ web/src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>About this app and its purpose.</p>
+    </div>
+  )
+}
+
+export default About
+
+
---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to the Mental Health & Wellness Platform.</p>
+    </div>
+  )
+}
+
+export default Home
+
+
